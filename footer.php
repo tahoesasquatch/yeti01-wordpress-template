52b7b5cfd30f05cc0eefc0111ae1d094f98da2e8
Przemyslaw Yeti Siedzinski <yeti@siedzinski.om>
2012-10-08 22:20:29 -0700

    First one

---
+++ footer.php
@@ -0,0 +1,21 @@
+<!-- begin footer -->
+</div>
+
+
+
+<p class="credit"><!--<?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. --> <cite><?php echo sprintf(__("Powered by <a href='http://wordpress.org/' title='%s'><strong>WordPress</strong></a>"), __("Powered by WordPress, state-of-the-art semantic personal publishing platform.")); ?></cite></p>
+
+</div>
+
+<?php wp_footer(); ?>
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-12850878-1");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+</body>
+</html>
